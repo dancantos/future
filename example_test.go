85e999080a25881d0de3dbe869b52d7bffa022e8
dancantos <dancantos@gmail.com>
2023-06-10 16:27:38 +1000

    more complicated package example

--- example_test.go
+++ example_test.go
@@ -27,3 +27,25 @@ func ExampleGoErr() {
 	// Output: value:
 	// err: Bad things
 }
+
+func Example() {
+	long1 := func() string {
+		return "input2"
+	}
+	long2 := func(string) (int, error) {
+		return 1, nil
+	}
+
+	future1 := future.Go(long1)
+	future2 := future.GoErr(func() (int, error) {
+		return long2(future1.Get())
+	})
+
+	if future2.Get().Err != nil {
+		fmt.Println("uh oh")
+	}
+
+	fmt.Printf("value: %d\n", future2.Get().Value)
+
+	// Output: value: 1
+}
